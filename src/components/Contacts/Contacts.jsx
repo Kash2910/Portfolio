e3ed918bb50674e1222fa33db7a182094733ee6e
Kash2910 <70763195+Kash2910@users.noreply.github.com>
2022-05-14 17:58:28 +0530

    Third Commit

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import './contacts.css'
+import { useRef } from 'react'
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+import {BsEnvelope} from 'react-icons/bs'
+import {FaHatWizard} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
 
-const Contacts = () => {
+
+const Contact = () => {
+  const form = useRef();
+  const [msg, setMsg] = useState("");
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_k21tg8s', 'template_8aq38hi', form.current, '5SzJoWUkRHKf1tpY1')
+    setMsg("Mensagem Enviada! Obrigado.")
+    e.target.reset()
+      // .then((result) => {
+      //     console.log(result.text);
+      // }, (error) => {
+      //     console.log(error.text);
+      // });
+  };
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5 className='text-light'>Get in Touch</h5>
+      <h2 className='text-light'>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <BsEnvelope className="contact__option-icon"/>
+            <h4>E-mail</h4>
+            <h5>kashif.sid2910@gmail.com</h5>
+            <a href="mailto:kashif.sid2910@gmail.com" >Sent an Email</a>
+          </article>
+          
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon"/>
+            <h4>WhatsApp</h4>
+            <h5>Click Me!</h5>
+            <a href="https://api.whatsapp.com/send?phone=918601948773" target="_blank">Send a Message</a>
+          </article>
+        </div>
+          <form ref={form} onSubmit={sendEmail}>
+            <input type="text" name="name" placeholder="Your Name" required />
+            <input type="email" name="email" placeholder="Your e-mail" required />
+            <textarea name="message" rows="7" required placeholder="Your message please :)"></textarea>
+            <button type="submit" className="btn btn-primary">SEND</button><span>{msg}</span>
+          </form>
+      </div>
+    </section>
   )
 }
 
-export default Contacts
+export default Contact
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './footer.css';
+import {GiCurlyWing} from 'react-icons/gi';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'><GiCurlyWing className='logoWing'/> MKS <GiCurlyWing className='logoWing logo-left'/> </a>
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>        
+    </footer>
   )
 }
 
